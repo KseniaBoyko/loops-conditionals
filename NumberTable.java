cb27cc158332f38aa2338dc0bbb4e151131255c2
WongPuiHei <77689556+WongPuiHei@users.noreply.github.com>
2021-03-23 02:44:11 -0700

    Add files via upload

---
+++ NumberTable.java
@@ -0,0 +1,15 @@
+/**
+ * Auto Generated Java Class.
+ */
+public class NumberTable {
+  
+  
+  public static void main(String[] args) 
+  { 
+    Table num= new Table();
+    num.getNumber();    
+  }
+  
+  /* ADD YOUR CODE HERE */
+  
+}
